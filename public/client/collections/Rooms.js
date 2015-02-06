471e37241e2b8c2883d7593167757acbdc3b897b
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-02-06 05:47:43 +0530

    Remove addRoom function

--- public/client/collections/Rooms.js
+++ public/client/collections/Rooms.js
@@ -1,16 +1,5 @@
 var Rooms = Backbone.Collection.extend({
 
-  model: Room,
-
-  //instantiate new model
-  //add model to collection
-  addRoom: function(room){
-    var room = new Room({
-      lang: room.lang,
-      room: room.room,
-      translations: room.translations
-    });
-    this.add(room);
-  }
+  model: Room
 
 });
